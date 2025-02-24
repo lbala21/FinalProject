186c04923c0a521064f4326c71d80e8435d8c935
Luka Balanchivadze <lbala21@freeuni.edu.ge>
2025-02-24 19:32:43 +0400

    Adding .py files

---
+++ finalProject/pos/infra/database.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+class Database:
+    def __init__(self, db_path="pos.db"):
+        self.db_path = db_path
+        self.connection = sqlite3.connect(self.db_path, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+        self._create_tables()
+
+    def _create_tables(self):
+        pass
+
+    def execute(self, query, params=()):
+        self.cursor.execute(query, params)
+        self.connection.commit()
+
+    def fetchall(self, query, params=()):
+        self.cursor.execute(query, params)
+        return self.cursor.fetchall()
+
+    def fetchone(self, query, params=()):
+        self.cursor.execute(query, params)
+        return self.cursor.fetchone()
