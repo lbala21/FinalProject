4f9dc3553a77c4f699da2b6ee88b0f8f0cd44823
Luka Balanchivadze <lbala21@freeuni.edu.ge>
2025-02-25 14:37:33 +0400

    change ReportService

--- finalProject/pos/core/services/report_service.py
+++ finalProject/pos/core/services/report_service.py
@@ -6,7 +6,7 @@ class ReportService:
     def __init__(self, report_repository: ReportRepository):
         self.report_repo = report_repository
 
-    def get_sales(self) -> Report:
+    def get_sales(self, shift_id: str) -> Report:
         """Generate a report based on closed receipts."""
-        sales = self.report_repo.generate()
+        sales = self.report_repo.generate(shift_id)
         return sales
