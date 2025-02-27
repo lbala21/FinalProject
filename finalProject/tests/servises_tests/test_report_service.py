d9648b8f8c47712fe4e97eee9a21ae89da8f2d44
Irakli Shelia <ishel21@freeuni.edu.ge>
2025-02-27 18:12:53 +0400

    added service tests

---
+++ finalProject/tests/servises_tests/test_report_service.py
@@ -0,0 +1,59 @@
+import unittest
+
+from pos.core.services.product_service import ProductService
+from pos.core.services.receipt_service import ReceiptService
+from pos.core.services.report_service import ReportService
+from pos.infra.database import Database
+from pos.infra.sqlite_repositories.campaign_sqlite_repository import CampaignSQLiteRepository
+from pos.infra.sqlite_repositories.product_sqlite_repository import ProductSQLiteRepository
+from pos.infra.sqlite_repositories.receipt_sqlite_repository import ReceiptSQLiteRepository
+from pos.infra.sqlite_repositories.report_sqlite_repository import ReportSQLiteRepository
+
+
+class TestReportService(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.db = Database()
+        self.report_repository = ReportSQLiteRepository(self.db)
+        self.report_service = ReportService(self.report_repository)
+        self.receipt_repository = ReceiptSQLiteRepository(self.db)
+        self.product_repository = ProductSQLiteRepository(self.db)
+        self.campaign_repository = CampaignSQLiteRepository(self.db)
+        self.receipt_service = ReceiptService(self.receipt_repository, self.product_repository,
+                                              self.campaign_repository)
+        self.product_service = ProductService(self.product_repository)
+
+    def tearDown(self) -> None:
+        self.db.execute("DROP TABLE IF EXISTS receipts")
+        self.db.execute("DROP TABLE IF EXISTS products")
+        self.db.execute("DROP TABLE IF EXISTS campaigns")
+
+    def test_generate_report_with_receipts(self) -> None:
+        self.receipt_service.create_receipt(receipt_id="1", shift_id="1")
+        self.product_service.create_product(product_id="1", name="apple", barcode="1234567890", price=5.0)
+        self.receipt_service.add_product_to_receipt(receipt_id="1", product_id="1", quantity=2)
+        self.receipt_service.close_receipt(receipt_id="1")
+
+        self.receipt_service.create_receipt(receipt_id="2", shift_id="1")
+        self.product_service.create_product(product_id="2", name="Banana", barcode="0987654321", price=7.0)
+        self.receipt_service.add_product_to_receipt(receipt_id="2", product_id="2", quantity=1)
+        self.receipt_service.close_receipt(receipt_id="2")
+
+        self.receipt_service.create_receipt(receipt_id="3", shift_id="2")
+        self.receipt_service.add_product_to_receipt(receipt_id="3", product_id="1", quantity=1)
+        self.receipt_service.add_product_to_receipt(receipt_id="3", product_id="2", quantity=1)
+        self.receipt_service.close_receipt(receipt_id="3")
+
+        self.receipt_service.create_receipt(receipt_id="4", shift_id="2")
+        self.product_service.create_product(product_id="45", name="book", barcode="0987600321", price=15.0)
+        self.receipt_service.add_product_to_receipt(receipt_id="4", product_id="45", quantity=1)
+
+        report = self.report_service.get_report(shift_id="1")
+        self.assertEqual(report.n_receipts, 2)
+        self.assertEqual(report.revenue, 17)
+        self.assertEqual(report.products, {"1":2, "2":1})
+
+        report = self.report_service.get_report(shift_id="2")
+        self.assertEqual(report.n_receipts, 1)
+        self.assertEqual(report.revenue, 12)
+        self.assertEqual(report.products, {"1":1, "2":1})
