7cefd88b15959f691d0b7d741008b2d719cf2727
Luka Balanchivadze <lbala21@freeuni.edu.ge>
2025-02-28 17:43:48 +0400

    update report_service.py

--- finalProject/pos/core/services/report_service.py
+++ finalProject/pos/core/services/report_service.py
@@ -1,12 +1,16 @@
 from pos.core.models.report import Report
-from pos.core.models.repositories import ReportRepository
+from pos.core.models.repositories import ReportRepository, ShiftRepository
 
 
 class ReportService:
-    def __init__(self, report_repository: ReportRepository):
+    def __init__(self, report_repository: ReportRepository, shift_repository: ShiftRepository):
         self.report_repo = report_repository
+        self.shift_repo = shift_repository
 
     def get_report(self, shift_id: str) -> Report:
         """Generate a report based on closed receipts."""
-        sales = self.report_repo.generate(shift_id)
-        return sales
+        shift = self.shift_repo.read(shift_id)
+        if shift is None:
+            raise ValueError("Shift not found.")
+        report = self.report_repo.generate(shift_id)
+        return report
